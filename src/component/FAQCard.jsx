2ce40078f9fb3987cfaa2f9706c195fefe58971e
arafat22184 <123alarafat@gmail.com>
2025-10-18 15:38:49 +0600

    responsive done in faq section

--- src/component/FAQCard.jsx
+++ src/component/FAQCard.jsx
@@ -17,17 +17,17 @@ const FAQCard = ({ faq }) => {
       initial={{ y: 100, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ duration: 1 }}
-      className={`py-4 px-8 rounded-lg space-y-3 ${
+      className={`py-3 px-4 lg:py-4 lg:px-8 rounded-lg space-y-1.5 xl:space-y-3 ${
         isOpen && "border border-[#253FB6] bg-[#F7F7F7]"
       }`}
     >
-      <div className="flex justify-between items-center">
-        <h5 className="text-[#192A48] font-medium text-2xl">
+      <div className="flex justify-between items-center gap-2">
+        <h5 className="text-[#192A48] font-medium text-xl lg:text-2xl">
           {id}. {question}
         </h5>
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className=" text-3xl cursor-pointer"
+          className="text-3xl cursor-pointer"
         >
           {isOpen ? (
             <AiFillMinusCircle className="text-[#253FB6]" />
@@ -36,7 +36,7 @@ const FAQCard = ({ faq }) => {
           )}
         </button>
       </div>
-      {isOpen && <p className="text-[#798090] text-lg">{ans}</p>}
+      {isOpen && <p className="text-[#798090] lg:text-lg">{ans}</p>}
     </motion.div>
   );
 };
